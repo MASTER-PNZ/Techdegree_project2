501377ab71438863f5e7d57d26451dd2500a5e33
MASTER-PNZ <paulkeesling@gmail.com>
2018-04-07 16:19:04 -0400

    Updated the showPage function
    Created clearer variables that represent the data to manipulate.
    The function now shows the first 10 students in the list but if I pass another page into the function it continues to show the same students.

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,31 @@
-//Selecting all of the student-items in the ul class of student-list
-//Selecting each individual student's information in the li class student-item
 
-function showPage(pageNumber,  ) {
-  const allStudents = $('.student-list');
-  const eachStudent = $('.student-item');
-  const pageNumber =
+//Selecting all individual student's information in the li class student-item
+//Getting the number of student-item elements
+//Setting the desired number of students to show per page
+//Setting the desired number of pages based on number of student elements
+//Setting initial page number to 0
+const eachStudent = $('.student-item');
+const studentNum = eachStudent.length;
+const studentsPerPage = 10;
+const numberOfPages = Math.ceil(studentNum / studentsPerPage);
+const pageNumber = 0;
+
+// A function to show 10 students on the page, hides all student-items,
+// and shows only the first 10.
+function showPage(pageNumber) {
     // first hide all students on the page
-    allStudents.hide();
+    eachStudent.hide();
     // Then loop through all students in our student list argument
-    allStudents.each(function(index, eachStudent){
-
+    eachStudent.each(function(index){
+      // if student should be on this page number
+      if (index <= studentsPerPage - 1) {
+        // show the student
+        $(this).show();
+        pageNumber += 1;
+      };
     });
-       // if student should be on this page number
-       if (eachStudent[] <= 10) {
-         // show the student
-
-       };
- }
+}
+showPage(1);
 
 function appendPageLinks(/* take a student list as an argument */) {
      // determine how many pages for this student list
