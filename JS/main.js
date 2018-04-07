affd023c1eba1d44586a4e7b45638ef1286c60de
MASTER-PNZ <paulkeesling@gmail.com>
2018-04-06 21:11:45 -0400

    Setting up functions
    Set showPage function to control number of students.
    created variables to store student ul and li class info for allStudents and eachStudent item.
    
    Set appendPagelinks function.

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,31 @@
 //Selecting all of the student-items in the ul class of student-list
+//Selecting each individual student's information in the li class student-item
 
-$('.student-list').hide();
+function showPage(pageNumber,  ) {
+  const allStudents = $('.student-list');
+  const eachStudent = $('.student-item');
+  const pageNumber =
+    // first hide all students on the page
+    allStudents.hide();
+    // Then loop through all students in our student list argument
+    allStudents.each(function(index, eachStudent){
+
+    });
+       // if student should be on this page number
+       if (eachStudent[] <= 10) {
+         // show the student
+
+       };
+ }
+
+function appendPageLinks(/* take a student list as an argument */) {
+     // determine how many pages for this student list
+     // create a page link section
+     // “for” every page
+         // add a page link to the page link section
+     // remove the old page link section from the site
+     // append our new page link section to the site
+     // define what happens when you click a link
+         // Use the showPage function to display the page for the link clicked
+         // mark that link as “active”
+ }
