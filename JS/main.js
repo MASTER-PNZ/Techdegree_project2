91a5256e6ae339bf3e2e6753a58d2c8a727a18f6
MASTER-PNZ <paulkeesling@gmail.com>
2018-04-07 21:19:32 -0400

    changed to a for loop
    attempting to add li html elements using append().

--- JS/main.js
+++ JS/main.js
@@ -2,12 +2,10 @@
 //Selecting all individual student's information in the li class student-item
 //Getting the number of student-item elements
 //Setting the desired number of students to show per page
-//Setting the desired number of pages based on number of student elements
 //Setting initial page number to 0
 const eachStudent = $('.student-item');
 const studentNum = eachStudent.length;
 const studentsPerPage = 10;
-const numberOfPages = Math.ceil(studentNum / studentsPerPage);
 const currentPageNumber = 0;
 
 // A function to show 10 students on the page, hides all student-items,
@@ -25,17 +23,36 @@ function showPage(currentPageNumber) {
       };
     });
 }
-showPage(1);
 
-function appendPageLinks(/* take a student list as an argument */eachStudent) {
+
+function appendPageLinks(/* take a student list as an argument */) {
      // determine how many pages for this student list
-     numberOfPages;
+     const numberOfPages = Math.ceil(studentNum / studentsPerPage);
      // create a page link section
+     $('.page').append('<div class="pagination"><ul></ul></div>');
      // “for” every page
+     for (currentPageNumber; currentPageNumber < numberOfPages; currentPageNumber += 1) {
          // add a page link to the page link section
+         if (currentPageNumber == 0) {
+           $('.pagination')
+           .append(
+             '<li><a class="active" href="#">' +
+             (currentPageNumber + 1) + '</a></li>');
+         } else {
+           $('.pagination')
+           .append('<li><a href="#">' +
+           (currentPageNumber + 1) + '</a></li>');
+         }
+     };
+      showPage(1);
      // remove the old page link section from the site
+
      // append our new page link section to the site
+
      // define what happens when you click a link
+     $('.pagination li a').on('click', function(){
          // Use the showPage function to display the page for the link clicked
+
          // mark that link as “active”
+     });
  }
